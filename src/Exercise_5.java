d5f3be56ade30051a05c1a1f3ef6dea34876f242
RomanShtoika <Roman.shtoika.tr.2021@lpnu.ua>
2023-05-30 18:12:41 +0300

    Lab3

---
+++ src/Exercise_5.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Exercise_502 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            // Зчитування границь проміжку та кроку
+            System.out.print("Введіть початкову границю проміжку: ");
+            double start = scanner.nextDouble();
+            System.out.print("Введіть кінцеву границю проміжку: ");
+            double end = scanner.nextDouble();
+            System.out.print("Введіть крок: ");
+            double step = scanner.nextDouble();
+
+            System.out.println("За допомогою оператора while:");
+
+            int countWhile = 0;
+            double xWhile = start;
+            while (xWhile <= end) {
+                double result = 0.25 * Math.pow(xWhile, 3) + xWhile - 1.2502;
+                System.out.println("f(" + xWhile + ") = " + result);
+                if (result >= -0.5 && result <= 0.5) {
+                    countWhile++;
+                }
+                xWhile += step;
+            }
+
+            System.out.println("Кількість значень функції, що знаходяться в діапазоні [-0.5, 0.5]: " + countWhile);
+            System.out.println();
+
+            System.out.println("За допомогою оператора for:");
+
+            int countFor = 0;
+            for (double xFor = start; xFor <= end; xFor += step) {
+                double result = 0.25 * Math.pow(xFor, 3) + xFor - 1.2502;
+                System.out.println("f(" + xFor + ") = " + result);
+                if (result >= -0.5 && result <= 0.5) {
+                    countFor++;
+                }
+            }
+
+            System.out.println("Кількість значень функції, що знаходяться в діапазоні [-0.5, 0.5]: " + countFor);
+        }
+    }
+
+
